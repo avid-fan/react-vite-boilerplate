e3ac30bbbda08fd22da95757385400d1f69c1fd1
Vladyslav Karpus <vladyslav.karpus@gmail.com>
2022-11-04 23:39:12 +0200

    chore: fix

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button type="button" onClick={() => setCount(count + 1)}>
-          count is
-          {count}
+          count is {count}
         </button>
         <p>
           Edit
