0667115fbd59b42e189e35c1dea133f114605f9b
Vladyslav Karpus <vladyslav.karpus@gmail.com>
2022-11-04 23:31:23 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const a = 5;
-console.log(a);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
